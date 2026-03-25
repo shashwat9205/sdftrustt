6409927c36edf699324e469128cfbe49391d9b10
shashwat9205 <shashwat.mishra1010101@gmail.com>
2026-03-25 13:13:17 +0530

    vite.js updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import {  ADMIN_BASE_URL } from "../config";
 
 // https://vite.dev/config/
 // vite.config.js
@@ -9,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/admin': {
-        target: 'https://sdftrustt.vercel.app/admin',
+        target: `${ADMIN_BASE_URL}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/admin/, ''),
       },
